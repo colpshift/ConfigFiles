3f46c1823b7774ba27ddddad4bf45ca2fb4ef110
colpshift <marcos.colpani@gmail.com>
2021-03-11 11:55:41 -0300

    11/03/2021 11:55 colps backup

--- arco-dwm/config.h
+++ arco-dwm/config.h
@@ -11,8 +11,8 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "JetBrainsMonoMedium Nerd Font Mono:size=9" };
-static const char dmenufont[]       = "JetBrainsMonoMedium Nerd Font Mono:size=9";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:style=Bold:size=9" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:style=Bold Italic:size=9";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -45,7 +45,7 @@ static const Rule rules[] = {
 	{ "Arcolinux-welcome-app.py",   NULL,     NULL,     0,          1,           -1 },
         { "Rambox",                     NULL,     NULL,     1 << 8,     0,           -1 },
         { "QML Timer",                  NULL,     NULL,     1 << 7,     0,           -1 },
-        { "Microsoft Teams - Preview",  NULL,     NULL,     1 << 8,     0,           -1 },
+        { "Microsoft Teams - Preview",  NULL,     NULL,     1 << 7,     0,           -1 },
         { "Galculator",                 NULL,     NULL,     0,          1,           -1 },
         { "qtpad",                      NULL,     NULL,     0,          1,           -1 },
 };

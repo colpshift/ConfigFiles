1c30586587bfb29874084f534191a95a2a9fb84e
colpshift <39013278+colpshift@users.noreply.github.com>
2021-08-21 11:01:09 -0300

    backup

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+--
+-- lua init file
+--
+
+require('pkgmgr') -- package manager settings
+require('settings') -- editor settings
+require('lsp') -- language server settings
+require('maps') -- maps settings
+

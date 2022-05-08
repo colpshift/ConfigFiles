164e05fea280475c4241a202833cfe216d9cb202
colpshift <39013278+colpshift@users.noreply.github.com>
2022-05-08 15:55:18 -0300

    backup

--- wezterm/wezterm/wezterm.lua
+++ wezterm/wezterm/wezterm.lua
@@ -6,47 +6,48 @@
 --
 local wezterm = require("wezterm")
 return {
-	-- font and size
-	--font = wezterm.font("Hasklug Nerd Font Mono"),
-	font_size = 10.5,
-	-- opacity and color theme
-	window_background_opacity = 0.80,
-	color_scheme = "iceberg-dark",
-	-- cursor
-	default_cursor_style = "SteadyBar",
-	-- tab
-	hide_tab_bar_if_only_one_tab = true,
-	-- padding
-	window_padding = {
-		left = 6,
-		right = 3,
-		top = 1,
-		bottom = 1,
-	},
-	-- keybinds
-	keys = {
-		-- SplitHorizontal
-		{
-			key = "v",
-			mods = "CTRL|ALT",
-			action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
-		},
-		-- SplitVertical
-		{
-			key = "h",
-			mods = "CTRL|ALT",
-			action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
-		},
-		-- CloseCurrentPane
-		{
-			key = "q",
-			mods = "ALT",
-			action = wezterm.action({ CloseCurrentPane = { confirm = true } }),
-		},
-	},
-	-- disable warnings
-	warn_about_missing_glyphs = false,
-	-- check updates
-	check_for_updates = true,
-	check_for_updates_interval_seconds = 86400,
+  -- font and size
+  font = wezterm.font("JetBrainsMono Nerd Font"),
+  font_size = 10.5,
+  harfbuzz_features = { "liga=1" },
+  -- opacity and color theme
+  window_background_opacity = 0.80,
+  color_scheme = "iceberg-dark",
+  -- cursor
+  default_cursor_style = "SteadyBar",
+  -- tab
+  hide_tab_bar_if_only_one_tab = true,
+  -- padding
+  window_padding = {
+    left = 6,
+    right = 3,
+    top = 1,
+    bottom = 1,
+  },
+  -- keybinds
+  keys = {
+    -- SplitHorizontal
+    {
+      key = "v",
+      mods = "CTRL|ALT",
+      action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+    },
+    -- SplitVertical
+    {
+      key = "h",
+      mods = "CTRL|ALT",
+      action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+    },
+    -- CloseCurrentPane
+    {
+      key = "q",
+      mods = "ALT",
+      action = wezterm.action({ CloseCurrentPane = { confirm = true } }),
+    },
+  },
+  -- disable warnings
+  warn_about_missing_glyphs = false,
+  -- check updates
+  check_for_updates = true,
+  check_for_updates_interval_seconds = 86400,
 }

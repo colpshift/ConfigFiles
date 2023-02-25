9901bfff7cee4171fcb8921e775abc9ba29d541a
colpshift <39013278+colpshift@users.noreply.github.com>
2023-02-25 16:28:51 -0300

    backup

---
+++ packages/workflow_packages.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+#
+## colps default apps
+#
+
+### Pin
+calendar
+contacts
+geary
+notion
+todoist
+rambox
+discord
+
+### office
+libreoffice
+okular
+xmind
+document scanner
+
+### internet
+firefox
+chrome
+
+### Audio
+Audacity
+Harmonoid
+Tidal Hi-Fi
+
+### Video
+VLC Media Player
+MPV
+Blender
+Guvcview
+Stremio
+
+### Image
+flameshot
+gimp
+darktable
+inkscape
+
+### code
+gedit
+github desktop
+neovim
+lunarvim
+vscode
+nodejs
+python
+rust
+go
+
+### Security
+bitwarden
+
+### utilities
+bleachbit
+ubuntu cleanner
+deja dup / timeshift
+caffeine
+copyq
+hplip
+balena etcher
+
+### terminal
+wezterm
+starship
+lsd
+ripgrep
+dust
+duf
+procs
+fd
+bat
+fasd
+broot
+fzf
+fzf-tab
+forgit
+emoji-cli
+highlight
+
+### gnome extensions
+"BackSlide" by p91paul
+"CPU Power Manager" by martin31821
+"Lock Keys" by kazimieras.vaina
+"Sound Input & Output Device Chooser" by kgshank
+"Vitals" by corecoding

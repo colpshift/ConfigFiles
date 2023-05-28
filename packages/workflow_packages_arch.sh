fc2896ea962537204df0837fd6fafb69d735423e
colpshift <39013278+colpshift@users.noreply.github.com>
2023-05-28 13:24:36 -0300

    current environment

---
+++ packages/workflow_packages_arch.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+#
+## colps default apps
+#
+
+### Pin
+browser plugin checker plus gmail 
+browser plugin checker plus gcalendar
+contacts gcontacts
+notion
+todoist
+whatsapp rambox app 
+discord
+telegram
+
+### office
+libreoffice
+xed
+xmind
+okular
+document scanner
+
+### internet
+firefox
+chrome
+edge
+
+### Audio
+Audacity
+Harmonoid
+Tidal Hi-Fi
+ncmpcpp
+
+### Video
+VLC Media Player
+MPV
+Blender
+Guvcview
+Stremio
+
+### Image
+flameshot
+gimp
+darktable
+inkscape
+
+### code
+gedit
+github desktop
+neovim
+nvchad
+vscode
+nodejs
+python
+rust
+go
+
+### Security
+bitwarden
+firewalld
+
+### utilities
+bleachbit
+deja dup / timeshift
+caffeine
+copyq
+hplip
+balena etcher
+
+### terminal
+wezterm
+starship
+lsd
+ripgrep
+lazygit
+go DiskUsage
+bottom
+node
+dust
+duf
+procs
+fd
+bat
+fasd
+broot
+fzf
+fzf-tab
+forgit
+emoji-cli
+highlight
+
+### gnome extensions
+"BackSlide" by p91paul
+"CPU Power Manager" by martin31821
+"Lock Keys" by kazimieras.vaina
+"Sound Input & Output Device Chooser" by kgshank
+"Vitals" by corecoding

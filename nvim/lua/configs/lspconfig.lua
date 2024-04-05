1b2d3794e866d24effa7a0958c2c4ff31a44c8e0
colpshift <39013278+colpshift@users.noreply.github.com>
2024-04-05 00:13:54 -0300

    backup

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "bashls", "jsonls", "tsserver" }
+local servers = { "html", "cssls", "bashls", "jsonls", "tsserver", "marksman", "lua_ls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do

c6c8eb8384a6e7df4e745ed6261886656495933c
colpshift <39013278+colpshift@users.noreply.github.com>
2024-09-20 07:06:35 -0300

    backup

---
+++ nvim/lazyvim_folder_structure.sh
@@ -0,0 +1,19 @@
+#
+#
+
+~/.config/nvim
+
+├── lua
+│ ├── config
+│ │ ├── autocmds.lua
+│ │ ├── keymaps.lua
+│ │ ├── lazy.lua
+│ │ └── options.lua
+│ └── plugins
+│ ├── spec1.lua
+│ ├── **
+│ └── spec2.lua
+└── init.lua
+
+#
+#

8b4e8826c1532362a50a49d248146c2ff1cf66a5
colpshift <39013278+colpshift@users.noreply.github.com>
2024-12-24 18:36:57 -0300

    backup

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,4 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---vim.opt.formatoptions = "a"
-vim.opt.colorcolumn = "81"
---vim.g.lazyvim_eslint_auto_format = true
+vim.o.statuscolumn = "%!v:lua.require'snacks.statuscolumn'.get()"
